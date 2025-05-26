ccb9ff5e2f26130e4c3b13cbfd4e61ceb400b3f3
AryanHaider72 <aryanhaider408@gmail.com>
2025-05-26 08:16:19 +0500

    New Commit

--- db.js
+++ db.js
@@ -2,14 +2,15 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const db = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
+
 
 db.getConnection((err, connection) => {
   if (err) {
